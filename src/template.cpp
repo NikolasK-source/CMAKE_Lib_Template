ae8d5a662aef4def7acba5e85b9b26f2a950cb65
Nikolas Koesling <nikolas@koesling.info>
2024-03-18 18:49:36 +0100

    update from CMAKE_Template

--- src/template.cpp
+++ src/template.cpp
@@ -4,10 +4,24 @@
  */
 
 #include "template.hpp"
+#include "CMAKE_Template_Lib_version_info.hpp"
 
 #include <iostream>
 
 int lib_main() {
     std::cout << "Hello from CMake Lib!!!\n";
+
+    std::cout << "PROJECT_VERSION: " << PROJECT_VERSION << '\n';
+    std::cout << "PROJECT_VERSION_MAJOR: " << PROJECT_VERSION_MAJOR << '\n';
+    std::cout << "PROJECT_VERSION_MINOR: " << PROJECT_VERSION_MINOR << '\n';
+    std::cout << "PROJECT_VERSION_PATCH: " << PROJECT_VERSION_PATCH << '\n';
+    std::cout << "PROJECT_NAME: " << PROJECT_NAME << '\n';
+    std::cout << "COMPILER_INFO: " << COMPILER_INFO << '\n';
+    std::cout << "SYSTEM_INFO: " << SYSTEM_INFO << '\n';
+    std::cout << "CPU_WORD_BYTES: " << CPU_WORD_BYTES << '\n';
+    std::cout << CMAKE_Template_Lib_version_info::NAME << ' ' << CMAKE_Template_Lib_version_info::VERSION_STR << ' '
+              << "Hello CMake!!!\n";
+    std::cout << "git commit: " << CMAKE_Template_Lib_version_info::GIT_HASH << '\n';
+
     return 0;
 }
